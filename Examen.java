58f07957fbeb769c3020d63d5f0bc479fa32728a
Alumno <alumno@localhost.localdomain>
2014-08-18 08:26:08 -0600

    primer commit

---
+++ Examen.java
@@ -0,0 +1,86 @@
+
+public class Examen {
+
+	
+	//devuelve x multiplicado por 2 (1pt)
+	//Aqui pues me pego dolor de cabeza al hacer este programa pero lo pude hacer. Ey aqui 
+	// Multiplo de DOS
+	int esmultiplicarPorDos(int x){
+		
+		int n1=1;
+		int n2=2;
+		{ 
+			System.out.println("Ingrese la multiplicacion:");
+		}
+		return n1 * n2;
+	}
+	
+	//devuelve el numero mayor entre a, b, c y d (2pts)
+	int getMayor(int a, int b, int c, int d)
+	{
+		int a1=1 ; 
+		int b1=2;
+		int c1=3;
+		int d1=4;
+		
+    
+		return -1;
+	
+	}
+	
+	//Pide 2 nombres de personas.
+	//Si los nombres son iguales imprime "Tocayos" de lo contrario imprime "No son tocayos". (2pts)
+	void sonTocayos()
+	{
+		
+	}
+	
+	//Imprime 100 numeros que sean multiplos de 5(3pts)
+	void imprimirMultiplosDe5()
+	{
+
+		int i=0 ;
+		int multi=100;  
+		while (i < 100);		
+		{
+			System.out.print("Los multiplos son:");		
+	}
+	
+	private boolean esMultiplo(Object multiplo, int x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	//Devuelve true si edad es menor o igual a 25 y promedio mayor o igual a 85 (3pts)
+	boolean aplicaBeca(int edad, int promedio)
+	{
+		return false;
+	}
+	
+	//Pide 4 numeros, si todos son pares devuelve true, de lo contrario devuelve false (4puntos)
+	// Ey! al fin me salio bueno uno!
+	// Me costo encontrar las funciones.
+	boolean sonPares()
+	{
+		int n1= 1; 
+		int n2=3;
+		int n3=4;
+		int n4=2;
+		{
+			if (n1>n2);
+				
+		}
+		{
+			System.out.print("Verdadero");
+		}
+				
+		return false;
+	}
+	
+	//Pueden probar sus funciones en el main pero no es necesario ni será evaluado
+	public static void main(String args[])
+	{
+		
+	
+}
+}
